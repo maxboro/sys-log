916b5ca1f30bbdbf5160527e9f2dac4c20522cd3
Max B. <maksym_borovyk@ukr.net>
2025-01-18 12:50:11 +0200

    Add used RAM

--- main.lua
+++ main.lua
@@ -6,3 +6,12 @@ function get_cpu()
     cpu_util = cpu_util:match("^%s*(.-)%s*$") -- Trim spaces
     return cpu_util
 end
+
+-- RAM used, in MB
+function get_ram()
+    local handle = io.popen("free -m | grep Mem | awk '{print $3}'")
+    local used_ram = handle:read("*a")
+    handle:close()
+    used_ram = used_ram:match("^%s*(.-)%s*$") -- Trim spaces
+    return used_ram
+end
