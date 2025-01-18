086bfbb9b61420e09168e489d928d0d095b2d1b9
Max B. <maksym_borovyk@ukr.net>
2025-01-18 13:51:41 +0200

    Create new file for each logging session

--- main.lua
+++ main.lua
@@ -20,41 +20,49 @@ end
 
 -- calculate tracked metrics
 function get_metric_values()
-    metrics = {}
+    local metrics = {}
     metrics["cpu_utilization"] = get_cpu()
     metrics["ram_utilization"] = get_ram()
     return metrics
 end
 
-function clean_logfile()
-    local logfile = io.open("logfile.log", "w")
+function clean_logfile(logfile_dest)
+    local logfile = io.open(logfile_dest, "w")
     logfile:close(logfile)
 end
 
-function write_to_logfile(line)
-    local logfile = io.open("logfile.log", "a")
+function write_to_logfile(logfile_dest, line)
+    local logfile = io.open(logfile_dest, "a")
     logfile:write(line.."\n")
     logfile:close()
 end
 
-function log(metrics)
+function log(logfile_dest, metrics)
     local metrics_string = string.format("CPU Utilization: %s%%, RAM Utilization: %s MB", metrics.cpu_utilization, metrics.ram_utilization)
     local timestamp = os.date().." | "
     local logline = timestamp..metrics_string
     print(logline)
-    write_to_logfile(logline)
+    write_to_logfile(logfile_dest, logline)
 end
 
-function exec_cycle_of_logging()
+function exec_cycle_of_logging(logfile_dest)
     local metrics = get_metric_values()
-    log(metrics)
+    log(logfile_dest, metrics)
+end
+
+function get_logfile_dest()
+    local logfile_name = "logfile_"..os.date("%Y-%m-%d__%H-%M-%S")..".log"
+    local logfile_dest = "./logs/"..logfile_name
+    print("Log will be saved to "..logfile_dest)
+    return logfile_dest
 end
 
 function main()
-    clean_logfile()
+    local logfile_dest = get_logfile_dest()
+    clean_logfile(logfile_dest)
     while (true)
     do
-        exec_cycle_of_logging()
+        exec_cycle_of_logging(logfile_dest)
         socket.sleep(1)
     end
 end
